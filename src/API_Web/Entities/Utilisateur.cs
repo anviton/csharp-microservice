29eba5036667bac346f27c1c6932ca777acfca39
Reynalde SEGERIE <resegerie@local.isima.fr>
2023-12-08 14:54:47 +0100

    Ajout de Utilisateur (Entities)

---
+++ src/API_Web/Entities/Utilisateur.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace utilisateur.Entities
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+        public String Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Email { get; set; }
+        public String Pass { get; set; }
+        public string NomComplet => Nom + " " + Prenom;
+
+        public Utilisateur(int id, string prenom, string nom, string email, string pass)
+        {
+            Id = id;
+            Prenom = prenom;
+            Nom = nom;
+            Email = email;
+            Pass = pass;
+        }
+
+        public void changePass()
+        {
+            this.Pass = "muchSecure";
+        }
+
+        public bool isPassSecure()
+        {
+            if (Pass.Length > 6)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
