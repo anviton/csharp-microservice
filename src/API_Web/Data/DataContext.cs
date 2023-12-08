caaa7a505885399b1586fac4b26d5ef2275d4454
Reynalde SEGERIE <resegerie@local.isima.fr>
2023-12-08 15:13:44 +0100

    Ajout Data repository

---
+++ src/API_Web/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+using utilisateur.Entities;
+
+namespace utilisateur.Data
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+
+        }
+
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+
+    }
+}
