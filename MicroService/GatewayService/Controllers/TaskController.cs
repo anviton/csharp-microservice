9d7f06a6759c82f0e3d889972be4e0479303cd17
LAPTOP-30NDNEC3\Simon <simon.dallet63@gmail.com>
2023-12-22 15:40:39 +0100

    Ajouter un controller pour le second micro service qui relaie les appel : get + get by id

--- MicroService/GatewayService/Controllers/TaskController.cs
+++ MicroService/GatewayService/Controllers/TaskController.cs
@@ -76,6 +76,61 @@ namespace GatewayService.Controllers
             }
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // Create an HttpClient instance using the factory
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                // Set the base address of the API you want to call
+                client.BaseAddress = new System.Uri("http://localhost:5002/");
+
+                // Send a POST request to the login endpoint
+                HttpResponseMessage response = await client.GetAsync("api/Tasks");
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // You can deserialize the response content here if needed
+                    var result = await response.Content.ReadFromJsonAsync<IEnumerable<TaskDTO>>();
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest("Get failed");
+                }
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            // Create an HttpClient instance using the factory
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                // Set the base address of the API you want to call
+                client.BaseAddress = new System.Uri("http://localhost:5002/");
+
+                // Send a POST request to the login endpoint
+                HttpResponseMessage response = await client.GetAsync("api/Tasks/" + id);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // You can deserialize the response content here if needed
+                    var result = await response.Content.ReadFromJsonAsync<TaskDTO>();
+                    return Ok(result);
+                }
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    // Task with the specified ID was not found
+                    return NotFound();
+                }
+                else
+                {
+                    return BadRequest("Get failed");
+                }
+            }
+        }
+
         
     }
 }
