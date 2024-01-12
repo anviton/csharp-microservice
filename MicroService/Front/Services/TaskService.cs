fff497451fb570220f6b12ad41af7e1a898acae9
Reynalde SEGERIE <resegerie@local.isima.fr>
2024-01-12 16:24:11 +0100

    Ajout bouton refresh tasks

--- MicroService/Front/Services/TaskService.cs
+++ MicroService/Front/Services/TaskService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Front.Components.Pages;
 using Front.Entities;
 
 namespace Front.Services
